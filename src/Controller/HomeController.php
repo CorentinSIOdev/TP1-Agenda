420ea865295b9e03400f5f7b764059e7b1656efe
corentin bonifacio <bonifaciocorentin@gmail.com>
2022-10-19 16:37:57 +0200

    Ajout du tableau de notre liste de contacts dans notre HomeController et notre vue Twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,9 +11,17 @@ class HomeController extends AbstractController
     // Fonction qui ouvre la page d'accueil dès le démarrage
     #[Route('/', name: 'home_demarrage')]
     public function home(): Response
-    {
-        return $this->render('home/index.html.twig', [
+    {   
+        // Tableau des contacts
+        $tableau = array(
+            // Affectation de la première ligne du tableau
+            "1"=>array("id"=>"1", "name"=>"Bonifacio", "firstName"=>"Corentin", "phone"=>"04 70 50 68 04"),
+        );
+
+        return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
+            // Renvoie le tableau à la vue avec le nom "tab".
+            'tab' => $tableau,
         ]);
     }
 }
