045e927f9fba908cb7f8c72dc55353aee8a162a7
corentin bonifacio <bonifaciocorentin@gmail.com>
2022-10-20 11:21:22 +0200

    Persistance de mon entity vers ma base de données

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +16,23 @@ class ContactController extends HomeController
         return $this->render('contact/contact.html.twig', [
             'controller_name' => 'ContactController',
         ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function createContact(ManagerRegistry $doctrine) {
+        $entityManager = $doctrine->getManager();
+
+        $contact = new Contact();
+        $contact->setNom(""); 
+        $contact->setPrenom("");
+        $contact->setTelephone("");
+        $contact->setAdresse("");
+        $contact->setVille("");
+        $contact->setAge("");
+
+        $entityManager->persist($contact);
+        $entityManager->flush();
+
+        return new Response($contact->getId());
     }
 }
